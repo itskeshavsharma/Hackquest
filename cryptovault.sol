eb596d674002bea3e9677165531480cdfac7bb6f
itskeshavsharma <keshavsharma7167@gmail.com>
2025-02-21 14:08:47 +0530

    Create cryptovault.sol

---
+++ cryptovault.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SavingsVault {
+    mapping(address => uint256) private balances;
+
+    function deposit() external payable {
+        require(msg.value > 0, "Deposit amount must be greater than zero");
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "Withdraw amount must be greater than zero");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return balances[msg.sender];
+    }
+}
